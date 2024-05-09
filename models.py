bdbba6278de56b483c0f12b196e3ef882cad97a3
satyendra123 <Singhsatyendra885@gmail.com>
2024-05-09 15:37:07 +0530

    Update and rename models.py to user

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length = 255)
-    address = models.CharField(max_length = 255)
-    fee = models.IntegerField()
